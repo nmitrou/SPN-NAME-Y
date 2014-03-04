bcc21eb818b8fc107d6a34853240cb79352977a4
Nick Mitrou <nmitrou@sfu.ca>
2014-03-03 19:30:46 -0800

    started master run code for SPN-NAME analysis

---
+++ SPN/Data/mat_files/SPN_NAME_Y_run_all.m
@@ -0,0 +1,5 @@
+%% SPN_NAME_Y Run All
+% collect data and put into matrices
+[CN_matrix_m,CN_matrix_t,PC_matrix_m,PC_matrix_t,PD_matrix_m,PD_matrix_t,Freq_matrix_m,Freq_matrix_t] = SPN_NAME_Y_datacollect;
+%% Get the bp/rbf/ls data
+
